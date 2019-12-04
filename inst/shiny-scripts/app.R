2ae8bec7629e498534b77b6d9b3b737a425bea62
Fanxing Bu <BFX3.1415@gmail.com>
2019-12-04 00:02:00 -0500

    update shiny app

--- inst/shiny-scripts/app.R
+++ inst/shiny-scripts/app.R
@@ -3,12 +3,13 @@ library(shiny)
 ui <- pageWithSidebar(
   headerPanel('Restriction Sites Mapping for Known DNA Sequence'),
   sidebarPanel(
-    textInput("dnaSeq", label = h3("DNA Sequence"), value = "GGCAGATTCCCCCTAACGTCGGACCCGCCCGCACCATGGTCAGGCATGCCCCTCCTCATCGCTGGGCACAGCCCAGAGGGT
+    textInput("dnaSeq", label = h4("DNA Sequence"), value = "GGCAGATTCCCCCTAACGTCGGACCCGCCCGCACCATGGTCAGGCATGCCCCTCCTCATCGCTGGGCACAGCCCAGAGGGT
     ATAAACAGTGCTGGAGGCTGGCGGGGCAGGCCAGCTGAGTCCTGAGCAGCAGCCCAGCGCAGCCACCGAGACACC
     ATGAGAGCCCTCACACTCCTCGCCCTATTGGCCCTGGCCGCACTTTGCATCGCTGGCCAGGCAGGTGAGTGCCCC"),
-    textInput("dnaName", label = h3("DNA name"), value = "Example DNA"),
-    textInput("targetSeq", label = h3("Target Sequence"), value = "ACGTCG"),
-    textInput("targetName", label = h3("Target Name"), value = "Example Target"),
+    textInput("dnaName", label = h4("DNA name"), value = "Example DNA"),
+    textInput("targetSeq", label = h4("Target Sequence"), value = "ACGTCG"),
+    textInput("targetName", label = h4("Target Name"), value = "Example Target"),
+    textInput("title", label = h4("Title"), value = "Restriction map around the target sequence"),
     numericInput('num', 'Number of Restriction Sites', 6,
                  min = 1, max = 10)
   ),
@@ -20,7 +21,7 @@ ui <- pageWithSidebar(
 server <- function(input, output, session) {
 
   observe({
-    rs <- findre(input$dnaName, input$dnaSeq, input$targetName, input$targetSeq, dataset = redata, num = input$num)
+    rs <- findre(input$dnaName, input$dnaSeq, input$targetName, input$targetSeq, dataset = redata, num = input$num, title = input$title)
     output$rsplot <- renderPlot(rs)
   })
 
